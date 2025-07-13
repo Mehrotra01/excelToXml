a91424037d02f2ad169deb501720913a4b462409
Harsh Mehrotra <harshmehrotra01@gmail.com>
2025-07-14 00:12:12 +0530

    xml

--- src/generator/xmlGenerator.ts
+++ src/generator/xmlGenerator.ts
@@ -6,19 +6,19 @@ import { markFileAsProcessed } from "../logger/processedFileLogs";
 
 function sanitizeFileName(fileName: string): string {
   return fileName
-    .replace(/[\\/:*?"<>|\s]+/g, "_") // Remove unsafe characters
+    .replace(/[\\/:*?"<>|\s]+/g, "_")
     .replace(/[^a-zA-Z0-9_.-]/g, "");
 }
 
 export async function generateXMLFile(rows: any[]): Promise<string[]> {
-  const outDir =  path.resolve("./output");
+  const outDir = path.resolve("./output");
   await fs.ensureDir(outDir);
   const allFiles: string[] = [];
 
   for (const row of rows) {
     const metadata = row.meta;
     const sanitizedFileName = sanitizeFileName(metadata.fileName);
-    const fileName = `${sanitizedFileName}-${row.formNbr || "unknown"}.xml`;
+    const fileName = `${sanitizedFileName}.xml`;
     const filePath = path.join(outDir, fileName);
 
     const root = create({ version: "1.0", encoding: "UTF-8" })
@@ -32,87 +32,38 @@ export async function generateXMLFile(rows: any[]): Promise<string[]> {
           "http://www.liquibase.org/xml/ns/dbchangelog http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-latest.xsd " +
           "http://www.liquibase.org/xml/ns/dbchangelog-ext http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-ext.xsd " +
           "http://www.liquibase.org/xml/ns/pro-mongodb http://www.liquibase.org/xml/ns/pro-mongodb/liquibase-pro-mongodb-latest.xsd " +
-          "http://www.liquibase.org/xml/ns/mongodb http://www.liquibase.org/xml/ns/mongodb/liquibase-mongodb-latest.xsd"
+          "http://www.liquibase.org/xml/ns/mongodb http://www.liquibase.org/xml/ns/mongodb/liquibase-mongodb-latest.xsd",
       });
 
     const changeSet = root.ele("changeSet", {
       id: `${metadata.changeSetId}_${row.formNbr}`,
-      author: "$(collection.name)"
+      author: "$(collection.name)",
     });
 
     if (metadata.operation === "insert") {
-      const fullDocument = {
-        formName: row.formName,
-        formNbr: row.formNbr,
-        editionDt: row.editionDt,
-        localPrintEligibleInd: row.localPrintElidgble,
-        optionalInd: row.optionalInd,
-        manualAmendmentInd: row.manualAmendInd,
-        manuscriptInd: row.manuscriptInd,
-        pullListInd: row.pullListInd,
-        mailingAdderssInd: row.mailingAdderssInd,
-        xyzInd: row.xyzInd,
-        effectiveDate: row.effectiveDate,
-        expirationDate: row.expirationDate,
-        lob: row.lob,
-        recipientTypes: row.rcpType,
-        sortingKeys: row.srtKey,
-        ...Object.fromEntries(
-          Object.entries(row).filter(
-            ([key]) =>
-              ![
-                "meta",
-                "formName",
-                "formNbr",
-                "editionDt",
-                "localPrintElidgble",
-                "optionalInd",
-                "manualAmendInd",
-                "manuscriptInd",
-                "pullListInd",
-                "mailingAdderssInd",
-                "xyzInd",
-                "effectiveDate",
-                "expirationDate",
-                "lob",
-                "rcpType",
-                "srtKey"
-              ].includes(key)
-          )
-        )
-      };
+      const fullDocument = { ...row };
+      delete fullDocument.meta;
 
       const insert = changeSet.ele("mongodb:insertOne", {
-        collectionName: "$(collection.name)"
+        collectionName: "$(collection.name)",
       });
       insert.ele("mongodb:document").txt(JSON.stringify(fullDocument, null, 2));
+
     } else if (metadata.operation === "update") {
       const update = changeSet.ele("mongodb:updateOne", {
-        collectionName: "$(collection.name)"
+        collectionName: "$(collection.name)",
       });
       update.ele("mongodb:filter").txt(
-        JSON.stringify(
-          {
-            formnbr: row.formNbr
-          },
-          null,
-          2
-        )
+        JSON.stringify({ formNbr: row.formNbr }, null, 2)
       );
       update.ele("mongodb:update").txt(
-        JSON.stringify(
-          {
-            $set: row.attributeToUpdate
-          },
-          null,
-          2
-        )
+        JSON.stringify({ $set: row.attributeToUpdate }, null, 2)
       );
     }
 
     const xml = root.end({ prettyPrint: true });
     await fs.writeFile(filePath, xml, "utf-8");
-    await markFileAsProcessed(`${sanitizedFileName}-${row.formNbr || ""}`);
+    await markFileAsProcessed(sanitizedFileName);
     allFiles.push(filePath);
   }
 
